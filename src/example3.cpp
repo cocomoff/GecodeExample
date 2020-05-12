9cfd7ba2c387162a974e72a81f9fa25fb8fe64ad
Keisuke Otaki <keisuke.otaki.jp@gmail.com>
2020-05-12 15:19:05 +0900

    min cost

--- src/example3.cpp
+++ src/example3.cpp
@@ -26,20 +26,28 @@ public:
 
         // post branching
         branch(*this, x, INT_VAR_SIZE_MIN(), INT_VAL_MIN());
-  }
-  // search support
-  SampleAD(SampleAD& s) : Space(s) {
-      x1.update(*this, s.x1);
-      x2.update(*this, s.x2);
-      x3.update(*this, s.x3);
-  }
-  virtual Space* copy(void) {
-      return new SampleAD(*this);
-  }
-  // print solution
-  void print(void) const {
-      std::cout << x1 << " " << x2 << " " << x3 << std::endl;
-  }
+    }
+
+    // search support
+    SampleAD(SampleAD& s) : Space(s) {
+        x1.update(*this, s.x1);
+        x2.update(*this, s.x2);
+        x3.update(*this, s.x3);
+    }
+    virtual Space* copy(void) {
+        return new SampleAD(*this);
+    }
+    virtual void constrain(const Space& _b) {
+        const SampleAD& b = static_cast<const SampleAD&>(_b);
+        IntVar b_x1(b.x1), b_x2(b.x2), b_x3(b.x3);
+        int cost = b_x1.val() + 10 * b_x2.val() + b_x3.val();
+        // rel(*this, x1 + 10 * x2 + x3 > cost);
+        rel(*this, x1 + 10 * x2 + x3 < cost);
+    }
+    // print solution
+    void print(void) const {
+        cout << x1 << " " << x2 << " " << x3 << endl;
+    }
 };
 
 // main function
@@ -47,6 +55,20 @@ int main(int argc, char* argv[]) {
     try {
         SampleAD* m = new SampleAD;
         DFS<SampleAD> e(m);
+        delete m;
+    
+        // search and print all solutions
+        while (SampleAD* s = e.next()) {
+            s->print(); delete s;
+        }
+    } catch (Exception e) {
+        cerr << "Exception: " << e.what() << endl;
+        return 1;
+    }
+
+    try {
+        SampleAD* m = new SampleAD;
+        BAB<SampleAD> e(m);
         delete m;
     
         // search and print all solutions
